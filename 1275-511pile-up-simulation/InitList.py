c744ab0948cb9681a1b23f31e62f41123e3b969b
DB_science <102671948+DB-science@users.noreply.github.com>
2025-05-19 15:48:25 +0200

    Create InitList.py

---
+++ 1275-511pile-up-simulation/InitList.py
@@ -0,0 +1,73 @@
+#*************************************************************************************************
+#** Copyright (c) 2025 Dr. Dominik Boras. All rights reserved.
+#** 
+#** Redistribution and use in source and binary forms, with or without modification, 
+#** are permitted provided that the following conditions are met:
+#**
+#** 1. Redistributions of source code must retain the above copyright notice
+#**    this list of conditions and the following disclaimer.
+#**
+#** 2. Redistributions in binary form must reproduce the above copyright notice, 
+#**    this list of conditions and the following disclaimer in the documentation 
+#**    and/or other materials provided with the distribution.
+#**
+#** 3. Neither the name of the copyright holder "Danny Petschke" nor the names of its  
+#**    contributors may be used to endorse or promote products derived from this software  
+#**    without specific prior written permission.
+#**
+#**
+#** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+#** OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+#** MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+#** COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+#** EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+#** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+#** HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+#** TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+#** EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#**
+#** Contact: dominik.boras@uni-wuerzburg.de
+#*************************************************************************************************
+
+class filterList:
+    dfCrossing = []
+    maxAmplitude = []
+    Area = []    
+    dfCrossing_GT= []
+    maxAmplitude_GT = []
+    Area_GT =[]
+    
+    PtR = []
+    PtD = []   
+    PtR_GT = []
+    PtD_GT = []
+    
+    RatioC = []
+    RatioC_GT = []
+    
+    
+    AreaFilter = 0
+    AreaFilter_GT = 0
+    
+    CFilter = 0
+    CFilter_GT = 0
+    
+    RCFilter = 0
+    RCFilter_GT = 0
+
+    amplitude = []
+    amplitude_GT = []
+
+    risetime = []
+    risetime_GT =[]
+
+    falltime = []
+    falltime_GT = []
+
+    ratio_rt_fl = []
+    ratio_rt_fl_GT = []
+
+    pulsewidth = []
+    pulsewidth_GT = []
+
+    
